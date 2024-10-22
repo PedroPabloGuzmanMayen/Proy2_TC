82a7ba0c870c284eb7d193b7416f6d3f2d6bba65
ppguz <ppguzmanmayen@gmail.com>
2024-10-22 12:16:24 -0600

    Chore: project structure

---
+++ src/CNF.py
@@ -0,0 +1,33 @@
+from Grammar import Grammar
+
+#Esta función debe eliminar 
+
+
+def eliminate_start_symbol(grammar):
+    grammar.non_terminals.insert
+def find_epsilon_productions():
+    pass
+
+def delete_epsilon_productions():
+    pass
+
+def find_non_binarized_expressions():
+    pass
+def binarize_expression():
+    pass
+
+def find_null_productions():
+    pass
+
+def delete_null_productions():
+    pass
+
+def find_unit_productions():
+    pass
+
+def delete_unit_productions():
+    pass
+
+
+def convert_to_cnf():
+    pass
---
+++ src/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def main():
+    list1 = [1, 2, 3, 4]
+    list2 = [5, 6, 7, 8]
+
+    array = np.array([list1, list2])
+
+    print(array)
+
+if __name__ == "__main__":
+    main()
