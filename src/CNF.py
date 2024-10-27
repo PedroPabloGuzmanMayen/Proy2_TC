0a62e50f58a89a7359717d32ed76aac745b5f447
gustavo <cru22779@uvg.edu.gt>
2024-10-26 18:22:55 -0600

    New: Eliminate start symbol func

--- src/CNF.py
+++ src/CNF.py
@@ -1,10 +1,26 @@
 from Grammar import Grammar
 
-#Esta función debe eliminar 
-
-
+'''
+Esta función debe eliminar el simbolo inicial (solo si es necesario) en las producciones
+'''
 def eliminate_start_symbol(grammar):
-    grammar.non_terminals.insert
+    if grammar.initial_symbol not in grammar.non_terminals:
+        return
+    new_initial_symbol = "S'"
+
+    while new_initial_symbol in grammar.non_terminals:
+        new_initial_symbol += "'"
+
+    grammar.productions[new_initial_symbol] == [[grammar.initial_symbol]]
+    grammar.initial_symbol == new_initial_symbol
+    
+    if grammar.initial_symbol in grammar.productions:
+        grammar.productions[new_initial_symbol].extend(grammar.productions[grammar.initial_symbol])
+
+    del grammar.productions[grammar.initial_symbol]
+'''
+Esta funcion buscara producciones que puedan derivar en la cadena vacia
+'''
 def find_epsilon_productions():
     pass
 
